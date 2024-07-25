619ae2b49a7bca2b6a78df0d360bab4231fd7766
Hannah Terblanche <126674584+Diana-Magus@users.noreply.github.com>
2024-07-25 15:44:18 +0200

    Added logic to display books using the index methods - Hannah

--- IndexExample/Program.cs
+++ IndexExample/Program.cs
@@ -4,19 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Book book = new Book("978-3-16-148410-0", "The Great Gatsby", "F. Scott Fitzgerald", "Scribner", 10.99);
+            Book book1 = new Book("9435903458304583050", "A Game Of Thrones", "George R R Martain", "HarperCollins Publishers UK", 350);
+            Book book2 = new Book("2005564838304183434", "The Last Olympian", "Rick Riordan", "Puffin Books", 280);
+            Book book3 = new Book("3248342093248923489", "Peter Pan", "J. M. Barrie ", "Puffin BooksPan Macmillan", 150);
+            List <Book> lstBook = new List<Book>();
 
-            Console.WriteLine(book[0]);
-            Console.WriteLine(book[1]);
-            Console.WriteLine(book[2]);
-            Console.WriteLine(book[3]);
-            Console.WriteLine(book[4]);
+            lstBook.Add(book1);
 
-            Console.WriteLine("ISBN: " + book["isbn"]);
-            Console.WriteLine("Title: " + book["title"]);
-            Console.WriteLine("Author: " + book["author"]);
-            Console.WriteLine("Publisher: " + book["publisher"]);
-            Console.WriteLine("Price: " + book["price"]);
+            lstBook.Add(book2);
+
+            lstBook.Add(book3);
+
+            foreach (Book book in lstBook)
+            {
+                Console.WriteLine("\nMethod 1:\n");
+
+                Console.WriteLine(book[0]);
+                Console.WriteLine(book[1]);
+                Console.WriteLine(book[2]);
+                Console.WriteLine(book[3]);
+                Console.WriteLine(book[4]);
+
+                Console.WriteLine("\nMethod 2:\n");
+
+                Console.WriteLine("ISBN: " + book["isbn"]);
+                Console.WriteLine("Title: " + book["title"]);
+                Console.WriteLine("Author: " + book["author"]);
+                Console.WriteLine("Publisher: " + book["publisher"]);
+                Console.WriteLine("Price: " + book["price"]);
+
+                Console.WriteLine("");
+            }
+
         }
     }
 }
